5ff0e265a7defed550089e7e37f7e4f297ff5d36
Asser Ayman <asser.ayman@gmail.com>
2019-07-24 12:18:43 +0200

    Data persistent using user defaults

--- ToDoHo/ToDoListViewController.swift
+++ ToDoHo/ToDoListViewController.swift
@@ -9,14 +9,20 @@
 import UIKit
 
 class ToDoListViewController: UITableViewController {
-
+    
+    //Array used to store the notes
     var itemArray = [String]()
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String]{
+            itemArray = items
+        }
     }
 
-    //MARK - TableView Data source methods
     
+    //MARK - TableView Data source methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -28,26 +34,30 @@ class ToDoListViewController: UITableViewController {
         return cell
     }
     
+    
     //MARK - TableView Delegate source methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
+        //Checking and uncheking the To-Do
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
         } else {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
 
         }
-
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
     
     //MARK - Add new Item
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         var newText = UITextField()
         let alert = UIAlertController(title: "New Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add", style: .default) { (action) in
+           
             //What will happen when user press add button
             self.itemArray.append(newText.text!)
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             self.tableView.reloadData()
         }
         alert.addAction(action)
@@ -56,7 +66,6 @@ class ToDoListViewController: UITableViewController {
             newText = textField
         }
         present(alert, animated: true, completion: nil)
-       
     }
 }
 
